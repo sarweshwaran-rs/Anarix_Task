8b6ad2bc0c60081b30ccf913fe270f43f8dbfe82
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-07-22 17:35:47 +0530

    Get rid of the configure and GenerativeModel function by removing and reinstalling the @google-generativeai library

--- llm/agent.py
+++ llm/agent.py
@@ -10,19 +10,21 @@ class AIAgent:
         if not gemini_api:
             raise ValueError("API Key not found in environment Variable")
         genai.configure(api_key=gemini_api) # type: ignore
-
         self.model = genai.GenerativeModel('gemini-2.5-flash') # type: ignore
 
     def generate_sql(self, question:str, table: str) -> str:
-        prompt = f""
-
-        {table} # type: ignore
-
+        prompt = f"""
+        You have access to the following database tables:
+        {table} 
+        Generate me the postgreSQL query for the following question:
         "{question}"
-
+        And ensure it is the valid for PostgreSQL and only return the SQL Query, no additional information.
+        """
         try:
             response = self.model.generate_content(prompt)
-            return response.text.strip()
+            query = response.text.strip()
+            query = query.replace('```sql','').replace('```','').strip()
+            return query
         except Exception as error:
             print(f"Error generating the SQL Query with LLM: {error}")
             return "Error: Could not generate SQL query"
