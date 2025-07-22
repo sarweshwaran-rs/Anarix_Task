afd01c848eb9abf93fb01db551ab71d7ad8b4ce0
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-07-22 08:04:01 +0530

    Added the init_db

--- db/init_db.py
+++ db/init_db.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
 def load_data(db_url, excel_path):
-    engine = create_engine(db_url)
+    url = os.getenv("DB_URL")
+    engine = create_engine(url)
     df = pd.read_excel(excel_path, sheet_name=None)
 
     for sheet, data in df.items():
