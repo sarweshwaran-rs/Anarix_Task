e757f87cfce6bd853eed1b89185f9feb87e8af5c
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-07-22 14:58:25 +0530

    Added the AI Agent

--- llm/agent.py
+++ llm/agent.py
@@ -7,26 +7,23 @@ load_dotenv()
 class AIAgent:
     def __init__(self):
         gemini_api = os.getenv("API_KEY")
-
         if not gemini_api:
-            raise ValueError(f"API Key not found in Environment")
-        genai.configure(api_key=gemini_api)
+            raise ValueError("API Key not found in environment Variable")
+        genai.configure(api_key=gemini_api) # type: ignore
 
-        self.model = genai.GenerativeModel('gemini-2.5-flash')
+        self.model = genai.GenerativeModel('gemini-2.5-flash') # type: ignore
 
-    def generate_sql(self, question: str, table: str) -> str:
-
+    def generate_sql(self, question:str, table: str) -> str:
         prompt = f""
 
-        {table}
+        {table} # type: ignore
 
         "{question}"
 
         try:
             response = self.model.generate_content(prompt)
             return response.text.strip()
-        except Exception as e:
-            print(f"Error in Generating the SQL query: {e}")
-            return "Error: Could not generate the SQL query"
+        except Exception as error:
+            print(f"Error generating the SQL Query with LLM: {error}")
+            return "Error: Could not generate SQL query"
         
-         
