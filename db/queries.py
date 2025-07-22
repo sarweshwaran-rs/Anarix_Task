b49fe0f8acdf693b070151077d4bfd30e692c975
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-07-22 18:29:44 +0530

    Added the logic to display the visual

--- db/queries.py
+++ db/queries.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from typing import List, Dict, Any
+#importing the matplotlib for plotting
+import matplotlib.pyplot as plt
 
 
 def execute_sql(db: Session, sql_query: str) -> List[Dict[str, Any]]:
@@ -19,3 +21,30 @@ def execute_sql(db: Session, sql_query: str) -> List[Dict[str, Any]]:
         print(f"Error executing SQL Query: {error}")
         print(f"Problematic Query: {sql_query}")
         return [{"error":str(error), "query":sql_query}]
+    
+def generate_img(result, graph_type="bar"):
+    data = list(result)
+
+    if not data:
+        print("No data to visualize")
+        return
+    
+    columns = list(data[0].keys())
+    x = [row[columns[0]] for row in data]
+    y = [row[columns[1]] for row in data]
+
+    plt.figure(figsize=(8,4))
+
+    if graph_type=="bar":
+        plt.bar(x,y)
+    elif graph_type=="line":
+        plt.plot(x,y)
+    elif graph_type=="pie":
+        plt.pie(y,labels=x)
+
+    plt.title("Graphical View of Result")
+    plt.xlabel(columns[0])
+    plt.ylabel(columns[1])
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
