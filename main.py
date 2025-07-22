c80e52bde675ce450721f5834cf1ed511d805263
Sarweshwaran R S <sarweshchandran@gmail.com>
2025-07-22 19:16:54 +0530

    Updated the main.py to visualize the response in charts

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from typing import List, Dict, Any
 
 from llm.agent import AIAgent
 from db.connection import get_db, get_all_table_schemas
-from db.queries import execute_sql
+from db.queries import execute_sql, generate_img
 
 import uvicorn
 import os
+from tabulate import tabulate
 
 app = FastAPI(
     title="E-Commerce AI Agent",
     description="An AI Agent Which answers the users Questions",
     version="0.1.0"
-    )
+)
 
 try:
     ai_agent = AIAgent()
@@ -54,6 +55,9 @@ async def ask_question(
         )
     
     print(f"Executing SQL: {generated_query}")
+    print(f"\n Question: {request.question}")
+    print(f"\nGenerated SQL Query: {generated_query}")
+
     query_result = execute_sql(db, generated_query)
 
     if any("error" in res for res in query_result):
@@ -61,6 +65,46 @@ async def ask_question(
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                            detail=f"Database query failed:{error_detail}. Generated SQL: {generated_query}" 
                         )
+    
+    #Response in the terminal
+    if query_result:
+        print("Query Result: ")
+        print(tabulate(query_result, headers="keys", tablefmt="pretty"))
+    else:
+        print("No Result Returned!")
+
+    #visualize
+    visual_keyword_map = {
+                            "bar":"bar",
+                            "chart":"bar",
+                            "graph":"bar",
+                            "plot":"bar",
+                            "visual":"bar",
+                            "graph":"bar",
+                            "compare":"bar",
+                            "line":"line",
+                            "trend":"line",
+                            "distribution":"line",
+                            "pie":"pie"
+                        }
+    
+    if query_result and len(query_result[0].keys()) >=2:
+        print("Attempting to show graph...")
+
+        l_ques = request.question.lower()
+        graph_type = None
+
+        for keyword,gtype in visual_keyword_map.items():
+            if keyword in l_ques:
+                graph_type = gtype
+                break
+        if graph_type:
+            try:
+                print(f"Visual keyword detected: {keyword} => Generating a '{graph_type}' chart")
+                generate_img(query_result,graph_type=graph_type)
+            except Exception as error:
+                print(f"Fail in Chart Generation: {error}")
+            
     return AnswerResponse(
         ori_question=request.question,
         sql_query_gen=generated_query,
